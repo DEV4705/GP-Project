f75f2a349d0a28196586b0b174549f3c1b7013c2
DEV4705 <davegameplay@gmail.com>
2025-02-10 11:47:00 +0700

    movement

--- Assets/Script/Movement.cs
+++ Assets/Script/Movement.cs
@@ -1,21 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using static UnityEngine.InputSystem.InputAction;
 
 public class Movement : MonoBehaviour
 {
+    private Rigidbody2D rb2d;
+    public float speed;
+    private void Awake()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
     public void SetDirection1(Vector2 direction)
     {
-
+        rb2d.velocity = direction * speed;
     }
 
     public void SetDirection(CallbackContext ctx)
     {
-        //Debug.Log("Move");
-        if(ctx.phase == UnityEngine.InputSystem.InputActionPhase.Performed)
+        Vector2 direction = ctx.ReadValue<Vector2>();
+        if(ctx.phase == UnityEngine.InputSystem.InputActionPhase.Performed || ctx.phase == UnityEngine.InputSystem.InputActionPhase.Canceled)
         {
             Debug.Log(ctx.ReadValue<Vector2>());
+            SetDirection1(direction);
         }
 
     }
